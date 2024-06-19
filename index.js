74fb09688386251448fb61ddff01dd5f77890789
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-19 13:23:33 +0600

    app connected with mongodb database

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json()); 
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://news-portal:6Y9DpPHU3ETwm6Jg@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.send("hello world")
